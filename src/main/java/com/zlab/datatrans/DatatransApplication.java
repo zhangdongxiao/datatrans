3c84fdbda5450c18d572af1dbf2cd522231812c4
zdongxiao <zdongxiao@foxmail.com>
2019-11-12 14:53:36 +0800

    no message

---
+++ src/main/java/com/zlab/datatrans/DatatransApplication.java
@@ -0,0 +1,13 @@
+package com.zlab.datatrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatatransApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatatransApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zlab/datatrans/DatatransApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlab.datatrans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatatransApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
