b3efd2f98f29243f53daba2cdeaa5092c82f190f
zdongxiao <zdongxiao@foxmail.com>
2019-11-13 16:04:19 +0800

    修改正则信息

--- src/main/java/com/zlab/busi/ConDataDeal.java
+++ src/main/java/com/zlab/busi/ConDataDeal.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -27,7 +29,7 @@ public class ConDataDeal {
 
     public void deal(Data data) throws Exception {
 
-        if(fileContentDeal == null){
+        if (fileContentDeal == null) {
             fileContentDeal = new FileContentDeal();//兼容单独的main启动
         }
 
@@ -45,16 +47,53 @@ public class ConDataDeal {
 
     public static void main(String[] args) throws Exception {
 
+        List<Data> datas = new ArrayList<>();
+
         Data in = new Data();
 
         in.setFilePath("/Users/dxz/Public/ProgramCodes/file/");
         in.setOutPut("/Users/dxz/Public/ProgramCodes/file/");
-        in.setFileName("crontab_20191112");
-        in.setOutPutFileName(in.getFileName()+".xls");
+        in.setFileName("crontab_206");
+        in.setOutPutFileName(in.getFileName() + ".xls");
+
+        datas.add(in);
+
+        Data in1 = new Data();
+
+        in1.setFilePath("/Users/dxz/Public/ProgramCodes/file/");
+        in1.setOutPut("/Users/dxz/Public/ProgramCodes/file/");
+        in1.setFileName("crontab_205");
+        in1.setOutPutFileName(in1.getFileName() + ".xls");
+
+        datas.add(in1);
+
+        Data in2 = new Data();
+
+        in2.setFilePath("/Users/dxz/Public/ProgramCodes/file/");
+        in2.setOutPut("/Users/dxz/Public/ProgramCodes/file/");
+        in2.setFileName("crontab_245");
+        in2.setOutPutFileName(in2.getFileName() + ".xls");
+
+        datas.add(in2);
+
+        Data in3 = new Data();
+
+        in3.setFilePath("/Users/dxz/Public/ProgramCodes/file/");
+        in3.setOutPut("/Users/dxz/Public/ProgramCodes/file/");
+        in3.setFileName("crontab_235");
+        in3.setOutPutFileName(in3.getFileName() + ".xls");
+
+        datas.add(in3);
 
         ConDataDeal conDataDeal = new ConDataDeal();
 
-        conDataDeal.deal(in);
+        datas.forEach(data -> {
+            try {
+                conDataDeal.deal(data);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
 
     }
 
